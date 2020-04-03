7a3e61b069b37141a64d4273d1aba483ec103d79
Richard Willis <richard@josephbeuysmum.co.uk>
2020-04-03 12:57:22 +0100

    Updates

---
+++ precache-manifest.ac49ed148b507aee7f9dd785a9cd090b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40f862c2dfbf8bd33b6095e3427253f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fe628c730c6e08c9165a",
+    "url": "/monsters-rolodex/static/css/main.bc2b284b.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe628c730c6e08c9165a",
+    "url": "/monsters-rolodex/static/js/main.8c67b8f7.chunk.js"
+  },
+  {
+    "revision": "83fef09b6b4b91e8e383",
+    "url": "/monsters-rolodex/static/js/runtime-main.6036d2a0.js"
+  }
+]);
